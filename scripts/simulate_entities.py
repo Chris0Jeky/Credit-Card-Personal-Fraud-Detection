691163ee6a7fd46fe2a5dd471b161ad34bdee64d
Chris0Jeky <jeky.tck@gmail.com>
2025-04-05 04:54:32 +0100

    Update simulate_entities.py

--- scripts/simulate_entities.py
+++ scripts/simulate_entities.py
@@ -14,3 +14,11 @@ OUTPUT_DIR = Path("../data/simulation_output")
 # Files to save
 ACCOUNT_DETAILS_FILE = OUTPUT_DIR / "simulated_account_details.json"
 TRANSACTIONS_FILE = OUTPUT_DIR / "simulated_account_transactions.csv"
+
+NUM_TRANSACTIONS_TO_SIMULATE = 75 # Generate a decent number of transactions
+MIN_ACCOUNT_AGE_DAYS = 60       # Account created between 60 days...
+MAX_ACCOUNT_AGE_DAYS = 365 * 2  # ...and 2 years ago
+MIN_DAYS_BEFORE_FIRST_TRANS = 1 # First transaction happens at least 1 day...
+MAX_DAYS_BEFORE_FIRST_TRANS = 30 # ...and up to 30 days after account creation
+# ---
+
